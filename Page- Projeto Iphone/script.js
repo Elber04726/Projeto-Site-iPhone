876d8a6a00671f0c8c9dc173fcc4dc5567958c50
ELBER LUIZ DA ROSA ARAUJO <109465693+Elber04726@users.noreply.github.com>
2023-06-08 23:36:18 -0300

    Add files via upload

---
+++ Page- Projeto Iphone/script.js
@@ -0,0 +1,25 @@
+function click1 (){
+    document.getElementById("icone").src = "https://i.postimg.cc/ZnkxqYnp/iphone-azul.png";
+    document.getElementById("imagem").src = "img/iPhone_azul_circulo.jpg";
+
+}
+
+function click2 (){
+    document.getElementById("icone").src = "https://i.postimg.cc/T3TQmP9c/iphone-amarelo.png";
+    document.getElementById("imagem").src = "img/iPhone_amarelo_circulo.jpg";
+}
+
+function click3 (){
+    document.getElementById("icone").src = "https://i.postimg.cc/pT5Bx5WP/iphone-preto.png";
+    document.getElementById("imagem").src = "img/iPhone_preto_circulo.jpg";
+}
+
+function click4 (){
+    document.getElementById("icone").src = "https://i.postimg.cc/7P7NWztV/iphone-vemelho.png";
+    document.getElementById("imagem").src = "img/iPhone_vermelho_circulo.jpg";
+}
+
+function click5 (){
+    document.getElementById("icone").src = "https://i.postimg.cc/0NQfWHhM/iphone-roxo.png";
+    document.getElementById("imagem").src = "img/iPhone_roxo_circulo.jpg";
+}
